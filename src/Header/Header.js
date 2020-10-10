84f87de5d09c3d75d084dc39d6776ec3b7f1c646
LeninReyez <devegus@gmail.com>
2020-10-09 23:39:17 -0500

    Add files via upload

---
+++ src/Header/Header.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+
+const Header = () => {
+    const [greeting, setGreeting] = useState(
+      'Hello'
+    );
+   
+    return (
+      <header>
+        <h1>{greeting}</h1>
+        <input
+          type="text"
+          value={greeting}
+          onChange={event => setGreeting(event.target.value)}
+        />
+      </header>
+    );
+  };
+
+  export default Header;
